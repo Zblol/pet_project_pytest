fe9b0757dad25f73d1ec0f568dcdb35f4974ff93
Vladislav Artemov <42673508+Zblol@users.noreply.github.com>
2021-05-17 20:57:56 +0300

    created Login Page class added test logic for Login Page class

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
 
     def __init__(self, browser, url):
@@ -6,3 +9,10 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, type_locator, locator):
+        try:
+            self.browser.find_element(type_locator, locator)
+        except NoSuchElementException:
+            return False
+        return True
